9d8ff5e9a79792199e272d12d39a41cf7b886a50
Alasdair Gray <alasdairthgray@gmail.com>
2026-08-12 18:17:04 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+import remarkBreaks from 'remark-breaks';
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	vite: {
+		plugins: [tailwindcss()]
+	},
+
+	integrations: [
+		mdx({
+			remarkPlugins: [remarkBreaks],
+			rehypePlugins: [],
+			extendMarkdownConfig: {
+				gfm: true
+			}
+		})
+	]
 });
